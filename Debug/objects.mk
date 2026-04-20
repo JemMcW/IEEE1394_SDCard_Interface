98d37992d6bd7371041857f2cc90c677c7137734
Nathan <njcolvin@protonmail.com>
2026-04-20 21:12:41 +1000

    ignore some files

--- Debug/objects.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-# Toolchain: local_ToolChain
-################################################################################
-
-USER_OBJS := /home/nathan/STM32CubeIDE/workspace/IEEE1394_Interface/HDL/build/top.o
-
-LIBS :=
-
